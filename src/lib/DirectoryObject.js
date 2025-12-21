4fa647df2ac5c61f318c0061d359ec9cab316e5c
gesslar <1266935+gesslar@users.noreply.github.com>
2025-12-21 14:32:27 -0500

    fixing .to

--- src/lib/DirectoryObject.js
+++ src/lib/DirectoryObject.js
@@ -459,6 +459,6 @@ export default class DirectoryObject extends FS {
     const thisPath = this.path
     const merged = FS.mergeOverlappingPaths(thisPath, newPath)
 
-    return new this(merged, this.temporary)
+    return new this.constructor(merged, this.temporary)
   }
 }
